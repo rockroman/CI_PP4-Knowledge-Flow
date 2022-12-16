990f1dbd7af4b17ba6b8d7826de979d18122de94
rockroman <rock.rakic@gmail.com>
2022-12-16 21:47:19 +0000

    Add allowed host variable value and make templates and static directories

--- knowledgeflow/settings.py
+++ knowledgeflow/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['knowledge-flow.herokuapp.com', 'localhost']
 
 
 # Application definition
