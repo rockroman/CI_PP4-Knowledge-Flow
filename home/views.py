0ac06c7145b312db62b53b3da0c53403b7a9b4fc
rockroman <rock.rakic@gmail.com>
2022-12-18 13:49:57 +0000

    create a basic class based view for homepage and add simple html template

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
 
+
 # Create your views here.
 
 
 class HomeView(generic.TemplateView):
     template_name = 'index.html'
 
+# def home(request):
+#     return render(request, "../templates/index.html")
