1bdcf95cbcddb6c96d2f2c912002079c89369b61
rockroman <rock.rakic@gmail.com>
2022-12-22 00:44:41 +0000

    Add Profie model that extends user model

--- siteusers/models.py
+++ siteusers/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 # Create your models here.
+
+
+class Profile(models.Model):
+    ROLE = (
+        ('Mentor', 'Mentor'),
+        ('Student', 'Student')
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = CloudinaryField('image', default='placeholder')
+    bio = models.TextField(max_length=1000, blank=True)
+    role = models.CharField(max_length=20, choices=ROLE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
