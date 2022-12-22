5c907253ce17838867eb2d3843b978d0ef104b1d
rockroman <rock.rakic@gmail.com>
2022-12-22 01:19:09 +0000

    Add functionality to create profile automaticaly for every user created

--- siteusers/apps.py
+++ siteusers/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SiteusersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'siteusers'
+
+    def ready(self):
+        import siteusers.signals
--- siteusers/signals.py
+++ siteusers/signals.py
@@ -5,3 +5,6 @@ from django.db.models.signals import post_save
 
 
 @receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
