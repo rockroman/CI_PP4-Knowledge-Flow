ddf9a55cca46e1b71526526872e4424e598409e6
rockroman <rock.rakic@gmail.com>
2022-12-22 13:31:30 +0000

    Add profile and user form

--- siteusers/forms.py
+++ siteusers/forms.py
@@ -1,3 +1,27 @@
 # use widget=forms.CheckboxSelectMultiple() for mentor
 # maybe widget=forms.RadioSelect() if only one should be selected but if later user needs to select one more think of way for this 
 # widget to be transformed to checkbox
+from django import forms
+from .models import Profile
+from django.contrib.auth.models import User
+
+
+class ProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['image', 'bio', 'role']
+        role = forms.MultipleChoiceField(
+            choices=ROLE, widget=forms.CheckboxSelectMultiple())
+
+    widgets = {
+        'bio': forms.Textarea(attrs={'class': 'form-control', 'rows': '10'}),
+    }
+
+
+class UserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email'] 
+
