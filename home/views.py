db81560c3b40d8f49520315b0ac1af92a15aae65
rockroman <rock.rakic@gmail.com>
2022-12-29 20:25:57 +0000

    Add skeleton for user profile page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import generic
+from django.contrib.auth.models import User
+from siteusers.models import Profile
 
 
 # Create your views here.
@@ -7,6 +9,19 @@ from django.views import generic
 
 class HomeView(generic.TemplateView):
     template_name = 'index.html'
+
+    # def get(self, request, *args, **kwargs):
+    #     user = request.user
+    #     profile = Profile.objects.all()
+    #     if user.is_authenticated is None:
+    #         return render(request, 'index.html')
+    #     if user.is_authenticated:
+    #         if user.profile:
+    #             return render(request, 'index.html')
+    #     else:
+    #         return redirect('create_myprofile')
+
+
 
 # def home(request):
 #     return render(request, "../templates/index.html")
