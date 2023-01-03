3a21cd1f79c283e3f8e14727955b6ec42ac8dedd
rockroman <rock.rakic@gmail.com>
2023-01-03 10:49:10 +0000

    fix navbar menu and authentication pages styling

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from .views import HomeView
 from . import views
 
 urlpatterns = [
-    # path('', views.HomeView.as_view(), name='home'),
-    path('', views.home, name='home'),
+    path('', HomeView.as_view(), name='home'),
+    # path('', views.home, name='home'),
     
 ]
--- home/views.py
+++ home/views.py
@@ -2,25 +2,15 @@ from django.shortcuts import render, redirect
 from django.views import generic 
 from django.contrib.auth.models import User
 from siteusers.models import Profile
-
+# from django.contrib.auth.mixins import UserPassesTestMixin
+# from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 
-
-# class HomeView(generic.TemplateView):
-#     if user.profile.role is None:
-#         template_name = 'create_profile.html'
-#     else:
-#         template_name = 'index.html'
-        
-    
-#     return render(request, template_name)
-   
- 
-
-def home(request):
+class HomeView( generic.TemplateView):
     template_name = 'index.html'
-        
-    return render(request, template_name)
+   
+
+
 
     
