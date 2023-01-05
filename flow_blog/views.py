7e15b18aa1207c86a652eb125f243e048b491bac
rockroman <rock.rakic@gmail.com>
2023-01-05 13:57:40 +0000

    Add templates dierctory to flow_blog app and create basic view

--- flow_blog/views.py
+++ flow_blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import BlogPost
+
 
 # Create your views here.
+class BlogPageView(generic.ListView):
+    model = BlogPost
+    template_name = 'blog.html'
