2bfa7d230342a45c5bf83c2828110e466b76fd1c
rockroman <rock.rakic@gmail.com>
2023-01-05 21:57:34 +0000

    add simple blog card to blog page and populate card with post model data

--- flow_blog/views.py
+++ flow_blog/views.py
@@ -6,4 +6,5 @@ from .models import BlogPost
 # Create your views here.
 class BlogPageView(ListView):
     model = BlogPost
+    context_object_name = 'blog_post'
     template_name = 'flow_blog/blog.html'
