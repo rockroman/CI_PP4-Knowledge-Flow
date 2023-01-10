7c1049927d6527410df42a537a05e0c621078767
rockroman <rock.rakic@gmail.com>
2023-01-10 11:35:15 +0000

    Add update blog view with coresponding template file and url path

--- flow_blog/urls.py
+++ flow_blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .models import BlogPost
-from .views import BlogPageView, BlogDetailView, AddBlogView
+from .views import BlogPageView, BlogDetailView, AddBlogView, UpdateBlogView
 
 urlpatterns = [
     path('', BlogPageView.as_view(), name='blog_page'),
-    path('blog/<int:pk>', BlogDetailView.as_view(), name='blog_details'),
-    path('add_blog/', AddBlogView.as_view(), name='add_blog'),
+    path('blog/blog/<int:pk>', BlogDetailView.as_view(), name='blog_details'),
+    path('blog/add_blog/', AddBlogView.as_view(), name='add_blog'),
+    path('blog/edit_blog/<int:pk>', UpdateBlogView.as_view(), name='edit_blog'),
 ]
--- flow_blog/views.py
+++ flow_blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import BlogPost
 from .forms import BlogForm
 
@@ -23,4 +23,9 @@ class AddBlogView(CreateView):
     model = BlogPost
     form_class = BlogForm
     template_name = 'flow_blog/add_blog.html'
-    
+
+
+class UpdateBlogView(UpdateView):
+    model = BlogPost
+    template_name = 'flow_blog/edit_blog.html'
+    fields = ['title', 'body', 'cover_image']
