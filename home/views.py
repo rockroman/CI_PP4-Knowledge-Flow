5236b17c5cf69c111ef660f36b0507528262db60
rockroman <rock.rakic@gmail.com>
2023-01-17 01:27:05 +0000

    style allauth login, sign up and logout page and add a new gero section

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import generic
 from django.contrib.auth.models import User
 from siteusers.models import Profile
+from flow_blog.models import BlogPost
 # from django.contrib.auth.mixins import UserPassesTestMixin
 # from django.contrib.auth.decorators import user_passes_test
 
@@ -10,3 +11,8 @@ from siteusers.models import Profile
 
 class HomeView(generic.TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['blogpost'] = BlogPost.objects.last()
+        return context 
