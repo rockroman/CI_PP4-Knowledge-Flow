09f4343c580108a69b6aa3db370be2a3ef0c122a
rockroman <rock.rakic@gmail.com>
2023-01-25 19:36:00 +0000

    create class based listview to display each category

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from . models import LearningCategory
+from flow_blog.models import BlogPost
 
 # Create your views here.
+
+
+class LearningCategoryListView(ListView):
+    template_name = 'category.html'
+    context_object_name = 'cat_list'
