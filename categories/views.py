09abf579e712ace11889207a9618f7c96b2f9fcb
rockroman <rock.rakic@gmail.com>
2023-01-26 15:52:48 +0000

    add view for Learning csaegories to be  available trough all templates and add it to context processor

--- categories/views.py
+++ categories/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from . models import LearningCategory
 from flow_blog.models import BlogPost
 
 # Create your views here.
 
 
-class LearningCategoryListView(ListView):
+class LearningCategoryListView(LoginRequiredMixin, ListView):
     template_name = 'categories/category.html'
     context_object_name = 'cat_list'
 
@@ -17,3 +18,11 @@ class LearningCategoryListView(ListView):
             'posts': BlogPost.objects.filter(category__name=self.kwargs['category'])
         }
         return content
+
+
+def list_of_categories(request):
+    category_list = LearningCategory.objects.all()
+    context = {
+        'category_list': category_list
+    }
+    return context
