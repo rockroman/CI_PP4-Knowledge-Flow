67a0349f6b006f2f648a7291df9876a8433593af
rockroman <rock.rakic@gmail.com>
2023-01-27 22:00:02 +0000

    add 2 fields to Learning category model fit category page layout

--- categories/models.py
+++ categories/models.py
@@ -7,7 +7,9 @@ from django.contrib.auth.models import User
 class LearningCategory(models.Model):
     maker = models.ForeignKey(User, on_delete=models.PROTECT)
     name = models.CharField(max_length=50)
+    start_quote = models.CharField(max_length=255, null=True)
     description = models.TextField()
+    importance_of_category = models.TextField(null=True)
 
     def __str__(self):
         return f'{self.name}'
