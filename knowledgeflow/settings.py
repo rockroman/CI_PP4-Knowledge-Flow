28fd0d22f2aac077adee859286c571209657c891
rockroman <rock.rakic@gmail.com>
2023-02-05 22:35:12 +0000

    fix horizontal scroll cause incorrect configurin bootstrap grid and add bottom hero text

--- knowledgeflow/settings.py
+++ knowledgeflow/settings.py
@@ -30,10 +30,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['knowledge-flow.herokuapp.com', 'localhost']
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']S
 
 
 # Application definition
