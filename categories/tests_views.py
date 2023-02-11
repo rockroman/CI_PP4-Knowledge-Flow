b2514f808b5e7de3451d728cf1ff9a009ceb23ea
rockroman <rock.rakic@gmail.com>
2023-02-11 14:34:24 +0000

    Finish testing flow_blog models

--- categories/tests_views.py
+++ categories/tests_views.py
@@ -1,1 +1,48 @@
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
+from .views import LearningCategoryListView
+from .models import LearningCategory, User
+from flow_blog.models import BlogPost
+
+
+class TestCategoryView(TestCase):
+    def setUp(self,**kwargs):
+        # creating and saving a new test user
+
+        self.user = User.objects.create(
+            username='testUser00',
+            password='mypass7999',
+            email='test@user99.com',
+            id='10',
+
+        )
+        self.user.save()
+        # creating test category
+        test_category = LearningCategory.objects.create(
+            maker=self.user,
+            name='Reading',
+            start_quote='Reading is the key'
+        )
+        test_category.save()
+        # content = {
+        #     'cat': 'Reading',
+        #     'posts': BlogPost.objects.filter(category__name='Reading')
+        # }        
+        # post = BlogPost.objects.filter(category__name='Reading') 
+
+
+        
+    # def test_the_response_code(self):
+    #     # creating a client
+    #     self.factory = RequestFactory()
+    #     request = self.factory.get('category/<category>/')
+    #     request.user = self.user
+    #     # qs = LearningCategoryListView.get_queryset(self)
+    #     response = LearningCategoryListView.as_view()(request)
+    #     self.assertEqual(response., 2)
+        
+
+    def test_is_the_right_template_used(self):
+        # creating a client
+        client = Client()
+        response = client.get('category/<category>/')
+        self.assertTemplateUsed('categories/category.html')
---
+++ flow_blog/tests_models.py
@@ -0,0 +1,47 @@
+from django.test import TestCase, Client
+from siteusers.models import Profile, User
+from .models import BlogPost, Comment
+
+
+# Create your tests here.
+class TestBlogModel(TestCase):
+
+    def test_string_method(self):
+        test_user = User.objects.create(username='miki')
+        blog = BlogPost.objects.create(
+            title='test title',
+            creator=test_user,
+        )
+        self.assertEqual(str(blog), 'test title | miki')
+
+    def test_get_absolute_url(self):
+        test_user = User.objects.create(username='Rock')
+        blog = BlogPost.objects.create(
+            title='test title',
+            creator=test_user,
+            cover_image='placeholder'
+        )
+        client = Client()
+        response = client.get(blog.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+
+
+class TestCommentModel(TestCase):
+
+    def test_model_string_method(self):
+        test_user = User.objects.create(username='miki')
+        blog = BlogPost.objects.create(
+            title='New Title',
+            creator=test_user,
+        )
+        comment = Comment.objects.create(
+            author=test_user,
+            blogpost=blog,
+            content='my new blog'
+
+        )
+        client = Client()
+        response = client.get('')
+        self.assertEqual(str(comment), 'miki|comment on New Title')
+
+ 
